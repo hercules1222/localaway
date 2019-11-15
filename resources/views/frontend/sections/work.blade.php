7b529dfe0ea2c34f9d6994cf5ebb4581990ff551
hercules <herculescoder@gmail.com>
2019-11-15 21:31:31 +0900

    landing page content changed

--- resources/views/frontend/sections/work.blade.php
+++ resources/views/frontend/sections/work.blade.php
@@ -19,27 +19,34 @@
                     <h1 class = "font-weight-bold color-orange my-auto mx-0">1</h1>
                     <img src="images/work1.jpg" alt="Image placeholder" >
                   </div>
-                  <div>
-                    <!-- <p class = "font-weight-bold text-center">Enter details about your trips or event.</p>  -->
-                    <p class = "text-center">Enter details about your style need or event.<br>Where are you going and how son do yo need it?</p>
+                  <div  class = "text-center">
+                    <span>Enter details about your style need or event.<br>
+                      <p class='font-weight-bold'>
+                        Where are you going and how son do yo need it?
+                      </p>
+                    </span>
                   </div>
               </div><div class="col-lg-4 col-md-12">
                   <div class = "d-flex text-center justify-content-center">
                     <h1 class = "font-weight-bold color-orange my-auto mx-0">2</h1>
                     <img src="images/work2.jpg" alt="Image placeholder" >
                   </div>
-                  <div>
-                    <!-- <p class = "font-weight-bold text-center">Enter details about your trips or event.</p>  -->
-                    <p class = "text-center">Tell us about your preferences.<br>Enter your size and fit preferences.</p>
+                  <div  class = "text-center">
+                    <span>Tell us about your preferences.<br>
+                      <p class='font-weight-bold'>Enter your size and fit preferences.</p>
+                    </span>
                   </div>
               </div><div class="col-lg-4 col-md-12">
                   <div class = "d-flex text-center justify-content-center">
                     <h1 class = "font-weight-bold color-orange my-auto mx-0">3</h1>
                     <img src="images/work3.jpg" alt="Image placeholder" >
                   </div>
-                  <div>
-                    <!-- <p class = "font-weight-bold text-center">Enter details about your trips or event.</p>  -->
-                    <p class = "text-center">Rentable styles delivered to you!<br>Try it on, wear it, return it. If you love something there is an option to purchase at a discount. Need to exchange? Its all local so its all easy.</p>
+                  <div  class = "text-center">
+                    <span><br>
+                      <p class='font-weight-bold'>
+                        Try it on, wear it, return it. If you love something there is an option to purchase at a discount. Need to exchange? Its all local so its all easy.
+                        </p>
+                      </span>
                   </div>
               </div>
             </div>
