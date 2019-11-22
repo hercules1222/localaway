038a78fc7ec9e3fdbf330d6f6962bafc1af79afb
hercules <herculescoder@gmail.com>
2019-11-22 10:14:15 -0800

    job page footer changed-2

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -44,7 +44,58 @@
     </div>
     </nav>
     <!-- END nav -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Welcome!</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Full name:</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="message-text" class="col-form-label">Email:</label>
+                  <input type="text" class="form-control" id="message-text">
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa2" data-whatever="@mdo" data-dismiss="modal">Request Access</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div class="modal fade" id="exampleModa2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Welcome!</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">WELCOME TO L/A!</label>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <a href="/survey">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModa2" data-whatever="@mdo">Request Access</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     <section class="site-hero-2 overlay" style="background-image: url(/images/newlanding/hero_3.jpg)" data-stellar-background-ratio="0.5" id="section-home">
         <div class="container-fluid h-100">
             <div class="row site-hero-inner-2 align-items-center h-100">
